720f072ddff8998d48e2df5738a4bb4dac049638
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-03-16 01:08:48 +0100

    Tests and app are running with different configs

--- src/main/java/com/carlosvin/covid/services/DataSourceExcel.java
+++ src/main/java/com/carlosvin/covid/services/DataSourceExcel.java
@@ -3,18 +3,18 @@ package com.carlosvin.covid.services;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-
-import javax.annotation.Resource;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -26,14 +26,21 @@ import com.carlosvin.covid.models.CovidDataEntry;
 public class DataSourceExcel implements DataSource {
 	private static final DateTimeFormatter DF = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	private final String baseUrl;
+	private final Clock clock;
 	
-	public DataSourceExcel(@Value("${base.url}") String baseUrl) {
+	@Autowired
+	public DataSourceExcel(@Value("${base.url}") String baseUrl, Clock clock) {
 		this.baseUrl = baseUrl;
+		this.clock = clock;
 	}
 
-	
-	private URL getUrl() throws MalformedURLException {
-		return new URL(String.format(baseUrl + DF.format(LocalDate.now()) + ".xls"));
+	private URL getUrl() {
+		String url = String.format(baseUrl + DF.format(LocalDate.now(clock)) + ".xls");
+		try {
+			return new URL(url);
+		} catch (MalformedURLException e) {
+			return getClass().getClassLoader().getResource(url);
+		}
 	}
 
 	@Override
@@ -42,8 +49,6 @@ public class DataSourceExcel implements DataSource {
 			Sheet sheet = wb.getSheetAt(0);
 			sheet.removeRow(sheet.getRow(0));
 			return StreamSupport.stream(sheet.spliterator(), false).map(r -> new CovidDataEntryExcel(r));
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
 		} catch (EncryptedDocumentException | InvalidFormatException e) {
 			throw new IOException(e);
 		}
--- src/test/java/com/carlosvin/covid/AppTest.java
+++ src/test/java/com/carlosvin/covid/AppTest.java
@@ -1,37 +1,56 @@
 package com.carlosvin.covid;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = { "url.base=classpath:data-" })
+@TestPropertySource(properties = { "base.url=data-" })
 class AppTest {
+	
+	@MockBean
+    private Clock clockMock;
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	/*
-	 * public String getBaseUrl() { URL url = getClass() .getClassLoader()
-	 * .getResource("COVID-19-geographic-disbtribution-worldwide-2020-03-15.xls");
-	 * String urlS = url.toString(); return urlS.substring(0, urlS.length() - 4); }
-	 */
 	
+	@BeforeEach
+	void setUp () {
+		Clock clock = Clock.fixed(Instant.parse("2020-03-14T10:10:30Z"), ZoneId.of("UTC"));
+		Mockito.when(clockMock.getZone()).thenReturn(clock.getZone());
+	    Mockito.when(clockMock.instant()).thenReturn(clock.instant());
+	    Mockito.when(clockMock.millis()).thenReturn(clock.millis());
+	}
+
 	@Test
-	void empty() throws Exception {
+	void getACountry() throws Exception {
 		this.mockMvc
 			.perform(get("/countries/ES"))
 			.andDo(print())
 			.andExpect(status().isOk());
-			//.andExpect(content().string("[]"));
+            //.andExpect(jsonPath("$.dates").isArray())
+            //.andExpect(jsonPath("$.dates", hasSize(75)));
 	}
 	
 	/*@Test
