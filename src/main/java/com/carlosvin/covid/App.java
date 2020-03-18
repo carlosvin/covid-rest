7330e6aec3e97baf988ee7d814d6b2037cefa79d
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-03-18 11:49:38 +0100

    use PORT environment variable

--- src/main/java/com/carlosvin/covid/App.java
+++ src/main/java/com/carlosvin/covid/App.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
 	public static void main(String[] args) {
+		String port = System.getenv("PORT");
+		System.setProperty("server.port", port == null || port.length() < 2 ? "8080" : port);
 		SpringApplication.run(App.class, args);
 	}
 
