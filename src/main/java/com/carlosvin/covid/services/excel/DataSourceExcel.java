2412425fb932425e07a12e4441963be1b7666510
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-03-19 14:15:29 +0100

    back to xlsx reader, because regular one consumes too much memory for google app engine

--- src/main/java/com/carlosvin/covid/services/excel/DataSourceExcel.java
+++ src/main/java/com/carlosvin/covid/services/excel/DataSourceExcel.java
@@ -1,19 +1,18 @@
 package com.carlosvin.covid.services.excel;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,28 +22,22 @@ import org.springframework.stereotype.Service;
 
 import com.carlosvin.covid.models.DateCountryStats;
 import com.carlosvin.covid.services.DataSource;
-import com.carlosvin.covid.services.DownloadManager;
+import com.monitorjbl.xlsx.StreamingReader;
 
 @Service
 @PropertySource("classpath:application.properties")
 public class DataSourceExcel implements DataSource {
-	private static String [] EXT = {".xls", ".xlsx"};
+	private static String [] EXT = {".xlsx", ".xls"};
 	private static final Logger LOG = LoggerFactory.getLogger(DataSourceExcel.class);
 	private static final DateTimeFormatter DF = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	
-	private final Path tmpPath;
 	private final String baseUrl;
 	private final Clock clock;
-	private final DownloadManager dm;
 	
 	@Autowired
-	public DataSourceExcel(@Value("${base.url.excel}") String baseUrl, Clock clock, DownloadManager dm) throws IOException {
+	public DataSourceExcel(@Value("${base.url.excel}") String baseUrl, Clock clock) throws IOException {
 		this.baseUrl = baseUrl;
 		this.clock = clock;
-		this.dm = dm;
-		File f = File.createTempFile("downloaded", ".dl");
-		f.deleteOnExit();
-		this.tmpPath = f.toPath(); 
 	}
 
 	public URL getUrl(int daysToSubtract, String ext) {
@@ -67,7 +60,7 @@ public class DataSourceExcel implements DataSource {
 				URL url = getUrl(daysToSubtract, ext);
 				return url.openStream();
 			} catch (IOException e) {
-				LOG.info("URL not found {}: {}", e.getMessage(), e.getCause());
+				LOG.info("URL not found {}", e.getMessage());
 			}
 		}
 		throw new IOException("Cannot open stream for any of configured URLs");
@@ -75,14 +68,21 @@ public class DataSourceExcel implements DataSource {
 	
 	@Override
 	public Stream<DateCountryStats> fetchData(int daysToSubtract) throws IOException {
-		long data = dm.download(openStream(daysToSubtract), tmpPath);
-		LOG.info("Saved {} bytes to {}", data, tmpPath);
-		try (Workbook wb = WorkbookFactory.create(tmpPath.toFile())) {
-			return StreamSupport
-					.stream(wb.getSheetAt(0).spliterator(), false)
-					.skip(1)
-					.map(r -> new CovidDataEntryExcel(r));
-			
+		
+		try (Workbook wb = StreamingReader.builder()
+		        .rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
+		        .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
+		        .open(openStream(daysToSubtract))) {
+			List<DateCountryStats> l = new LinkedList<DateCountryStats>();
+
+			for (Row r: wb.getSheetAt(0)) {
+				try {
+					l.add(new CovidDataEntryExcel(r));
+				} catch (Exception e) {
+					LOG.info("Ignoring row with invalid format: ", r);
+				}
+			}
+			return l.stream();
 		}
 	}
 
