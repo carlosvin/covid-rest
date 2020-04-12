cc003010cf9bc9c4b330e17c8c122d35c7f96d98
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-04-12 12:32:34 +0200

    enable scheduling

--- src/main/java/com/carlosvin/covid/App.java
+++ src/main/java/com/carlosvin/covid/App.java
@@ -3,10 +3,12 @@ package com.carlosvin.covid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableScheduling
 public class App {
 
 	public static void main(String[] args) {
